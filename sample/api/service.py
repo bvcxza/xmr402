d5a26d6bc1867e83d599ed14ce71941b086106b2
bvcxza <175357591+bvcxza@users.noreply.github.com>
2025-01-09 07:41:45 -0300

    cache control

--- sample/api/service.py
+++ sample/api/service.py
@@ -3,8 +3,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 class CORSRequestHandler(SimpleHTTPRequestHandler):
 	def end_headers(self):
 		self.send_header('Access-Control-Allow-Origin', '*')
-#		self.send_header('Access-Control-Allow-Headers', 'Authorization')
-#		self.send_header('Access-Control-Allow-Credentials', 'true')
+		self.send_header('Cache-Control', 'no-cache')
 		super().end_headers()
 
 test(CORSRequestHandler, HTTPServer, port=8888)
